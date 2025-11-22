e2013edcef2b2ed9d98c4493bbb7ea63d9b4238f
Joe Hans <joseph.hans@gmail.com>
2025-11-22 11:11:48 -0500

    first commit

---
+++ reverse_string.py
@@ -0,0 +1,5 @@
+def reverse_string():
+    """Reads a string from user input and prints the reversed string."""
+    user_input = input("Enter a string: ")
+    reversed_string = user_input[::-1]
+    print("Reversed string:", reversed_string)
