fa7d1cf39ba7745efb592238cb2abb428d55da07
Joe Hans <joseph.hans@gmail.com>
2025-11-22 12:24:14 -0500

    first commit

---
+++ find_max.py
@@ -0,0 +1,24 @@
+def find_max(numbers):
+    """Returns the maximum number from a list of numbers."""
+    max_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+    return max_num
+if __name__ == "__main__":
+    sample_numbers = [-10, 0, 5, 3, 99, -5]
+    print("The maximum number is:", find_max(sample_numbers))
+
+
+def find_max(numbers):
+    """Returns the maximum number from a list of numbers."""
+    if not numbers:
+        return None
+    max_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+    return max_num
+if __name__ == "__main__":
+    sample_numbers = []
+    print("The maximum number is:", find_max(sample_numbers))
